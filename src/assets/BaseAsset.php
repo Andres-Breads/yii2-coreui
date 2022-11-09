52776856dd0126e2209745f755aa222ae033bc2f
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2022-11-09 10:21:19 -0500

    assets added

---
+++ src/assets/BaseAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace andresbreads\coreui\assets;
+
+use yii\web\AssetBundle;
+
+class BaseAsset extends AssetBundle
+{
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset',
+        'yii\bootstrap5\BootstrapPluginAsset'
+    ];
+}
---
+++ src/assets/CoreUiAsset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace andresbreads\coreui\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class CoreUiAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/coreui/coreui/dist';
+    public $css = [
+        "css/coreui.min.css",
+    ];
+    public $js = [
+        "js/coreui.min.js",
+    ];
+    public $depends = [
+        BaseAsset::class
+    ];
+}
